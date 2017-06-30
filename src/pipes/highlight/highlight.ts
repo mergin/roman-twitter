b6bbef6ec4e15a091582e23589e98a1532bfe6b3
David <david.prieto@synergy-gb.com>
2017-06-30 11:17:18 -0400

    Added pipe to highlight mentions and hashtags.

--- src/pipes/highlight/highlight.ts
+++ src/pipes/highlight/highlight.ts
@@ -2,21 +2,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 /**
  * Generated class for the HighlightPipe pipe.
- *
- * See https://angular.io/docs/ts/latest/guide/pipes.html for more info on
- * Angular Pipes.
+ * Highlights hashtags and mentions
  */
 @Pipe({
   name: 'highlight',
 })
 export class HighlightPipe implements PipeTransform {
-	transform(text: string, search): string {
-	    var pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-	    pattern = pattern.split(' ').filter((t) => {
-	    	return t.length > 0;
-	    }).join('|');
-	    var regex = new RegExp(pattern, 'gi');
+	transform(text: string): string {
+	    var regex = /((^|\s)((#)[a-z\d-]+))|(\B@[a-z0-9_-]+)/gi;
+	    text = text.replace(regex, (match) => `<span class="highlight">${match}</span>`);
+	    text = '<p class="tweet-text">'+text+'</p>';
 
-	    return search ? text.replace(regex, (match) => `<span class="highlight">${match}</span>`) : text;
+	    return text;
 	}
 }
